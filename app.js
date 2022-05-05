ed81ff8eab19f0ce8a9950351c309bceac238570
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-05 23:06:11 +0900

    [Add]マウス操作のイベント

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000ff'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#ff0000'});
+//   });
+// });
+
+// $('.セレクタ名').イベント名(function(){
+//   イベント発生時に行われる処理
+// });
+
+
+
+// 【マウスクリックイベントの記述】
+// $('.セレクタ名').on('click', function() {
+//   イベント発生時に行われる処理
+// });
+
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({'background-color': '#0000ff', 
-    'width': ' 200px',
-    'height': '100px'
-  }).slideUp(1000);
- });
-});
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext')
+  })
+})
